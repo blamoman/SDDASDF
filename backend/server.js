76be5c0ca09811ec86e67c6f6ed6c2a3832e32cd
jv <jv@localhost.localdomain>
2019-05-30 12:08:21 +0100

    dfs

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const url = "mongodb://admin:pass@mongodb-34-centos7:27017/admin";
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
   http.createServer((req, res) => {
+console.log(req.url);
+console.log(req.method);
+console.log(req.url === "/rando" && req.method === "GET");
      if(req.url === "/rando" && req.method === "GET") {
 console.log("here");
        const collection = db.collection('test');
