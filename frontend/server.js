a9adddc251c44f456d4eb7123d95e2dadf51b9d4
jv <jv@localhost.localdomain>
2019-05-30 14:11:29 +0100

    sadfr

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,22 @@ const http = require("http");
 const fs = require("fs");
 
 http.createServer((req, res) => {
-	const myhtml = fs.readFileSync("./wowgreatcss.html");
-	res.end(myhtml);
+    if (req.url === "/rando") {
+        const toReq = {
+            hostname: "sddasdf",
+            port: 8000,
+            path: "/rando",
+            method: "GET"
+        };
+        const backendReq = http.request(toReq, resp => {
+            let body = "";
+            resp.on("data", chunk => (body += chunk));
+            resp.on("end", () => res.end(body));
+        });
+        backendReq.end();
+    } else {
+        const myhtml = fs.readFileSync("./wowgreatcss.html");
+        res.end(myhtml);
+    }
 }).listen(8000);
+
