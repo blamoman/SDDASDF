5f1dc520bd8265dac2d7c9cd9a0aceb4842c18be
jv <jv@localhost.localdomain>
2019-05-30 11:57:33 +0100

    try again

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ const http = require("http");
 const url = "mongodb://admin:pass@mongodb-34-centos7:27017/admin";
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
-console.log(db);
   http.createServer((req, res) => {
+     console.log(req.path);
+     console.log(req.method);
+     res.end("yo yo");
      if(req.path === "/rando" && req.method === "GET") {
        const collection = db.collection('test');
        collection.find({}).toArray((err, docs) => {
