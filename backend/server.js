8e93c808edae4500ce8d54483ec671cb1c266145
jv <jv@localhost.localdomain>
2019-05-30 12:35:58 +0100

    fds

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ const url = "mongodb://admin:pass@mongodb-34-centos7:27017";
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     http.createServer((req, res) => {
- 	console.log("here1");
+        console.log("here1");
         console.log(client);
         if (req.url === "/rando" && req.method === "GET") {
             console.log("here2");
-	    const db = client.db("admin");
-	    const collection = db.collection("test");
-            console.log(collection);
+            const db = client.db("admin");
+            const collection = db.collection("test");
+            collection.find({}).toArray((err, docs) => console.log(docs));
             res.end("donezo");
         }
     }).listen(8000);
