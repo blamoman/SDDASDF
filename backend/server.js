3a6198d6a704bb1516260a53e767d2a1d7bdc967
jv <jv@localhost.localdomain>
2019-05-30 12:23:40 +0100

    fds

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const MongoClient = require("mongodb").MongoClient;
 const http = require("http");
-const url = "mongodb://admin:pass@mongodb-34-centos7:27017/admin";
+const url = "mongodb://admin:pass@mongodb-34-centos7:27017";
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     http.createServer((req, res) => {
-        console.log(req.url);
-        console.log(req.method);
-        console.log(req.url === "/rando" && req.method === "GET");
+ 	console.log("here1");
+        console.log(client);
         if (req.url === "/rando" && req.method === "GET") {
-            console.log("here");
+            console.log("here2");
             try {
                 const db = client.db("admin");
                 const collection = db.collection("test");
